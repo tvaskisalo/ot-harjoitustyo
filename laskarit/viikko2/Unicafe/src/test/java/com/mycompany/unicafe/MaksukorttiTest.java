1c6f6cab034c749532358b0fe7d99ffd19454d58
tvaskisalo <61991314+tvaskisalo@users.noreply.github.com>
2020-03-23 18:07:58 +0200

    Laskareiden kohdat 1-4

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,42 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void luodunKortinSaldoOnOikein() {
+        assertEquals(10,kortti.saldo());
+    }
+    
+    @Test
+    public void saldonKavattaminenLisaaSaldoa1() {
+        kortti.lataaRahaa(1000);
+        assertEquals("saldo: 10.10", kortti.toString());
+    }
+    
+    @Test
+    public void saldonKavattaminenLisaaSaldoa2() {
+        kortti.lataaRahaa(1111);
+        assertEquals("saldo: 11.21", kortti.toString());
+    }
+    
+    @Test
+    public void saldoVaheneeKunRahaaOnTarpeaksi() {
+        kortti.otaRahaa(9);
+        assertEquals(1, kortti.saldo());
+    }
+    
+    @Test
+    public void saldoEiMuutuJosRahaEiRiita() {
+        kortti.otaRahaa(11);
+        assertEquals("saldo: 0.10",kortti.toString());
+    }
+    
+    @Test
+    public void palauttaaFalseKunRahaEiRiita() {
+        assertFalse(kortti.otaRahaa(11));
+    }
+    @Test
+    public void palauttaaTrueKunRahaRiitaa() {
+        assertTrue(kortti.otaRahaa(10));
+    }
 }
