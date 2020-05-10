b51a3e359a85201ca425ca6a8e5c97f9d0b5911e
tvaskisalo <61991314+tvaskisalo@users.noreply.github.com>
2020-05-10 18:33:52 +0300

    Checkstyle-virheiden korjailua

--- ExerciseApp/src/main/java/exerciseapp/dao/Database.java
+++ ExerciseApp/src/main/java/exerciseapp/dao/Database.java
@@ -22,7 +22,7 @@ public class Database implements DataDao {
      */
     public Database(String filename) {
         try {
-            conn = DriverManager.getConnection("jdbc:sqlite:"+filename+".db");
+            conn = DriverManager.getConnection("jdbc:sqlite:" + filename + ".db");
             Statement s1 = conn.createStatement();
             s1.execute("CREATE TABLE Users (id INTEGER PRIMARY KEY, username TEXT UNIQUE, exercises INTEGER, correct INTEGER)");
             Statement s2 = conn.createStatement();
@@ -36,6 +36,7 @@ public class Database implements DataDao {
      * Metodi asettaa käyttäjänimen, jolla haetaan ja muutetaan tietokannasta dataa
      * @param username Käyttäjän antama käyttäjänimi 
      */
+    @Override
     public void setUsername(String username) {
         this.username = username;
     }
@@ -43,6 +44,7 @@ public class Database implements DataDao {
     /**
      * Metodi luo uuden käyttäjän tietokantaan
      */
+    @Override
     public void createUser() {
         try {
             PreparedStatement ps = conn.prepareStatement("INSERT INTO Users (username, exercises, correct) VALUES (?,0,0)");
@@ -58,6 +60,7 @@ public class Database implements DataDao {
      * Metodi hakee tietokannasta jo asetetun käyttäjän pisteet.
      * @return Käyttäjän pisteet
      */
+    @Override
     public int getPoints() {
         try {
             PreparedStatement ps = conn.prepareStatement("SELECT correct FROM Users WHERE username=?");
@@ -75,6 +78,7 @@ public class Database implements DataDao {
     /**
      * Metodi kasvattaa yhdellä käyttäjän pisteitä tietokannassa
      */
+    @Override
     public void addPoints() {
         int points = getPoints();
         try {
@@ -92,6 +96,7 @@ public class Database implements DataDao {
      * Metodi palauttaa, kuinka monta tehtävää käyttäjä on tehnyt
      * @return  Käyttäjän tehtyjen tehtävien määrä
      */
+    @Override
     public int getExerciseCount() {
         try {
             PreparedStatement ps = conn.prepareStatement("SELECT exercises FROM Users WHERE username=?");
@@ -109,6 +114,7 @@ public class Database implements DataDao {
     /**
      * Metodi kasvattaa yhdellä tehtyjen tehtävien määrää.
      */
+    @Override
     public void increaseExerciseCount() {
         int count = getExerciseCount();
         try {
@@ -126,6 +132,7 @@ public class Database implements DataDao {
      * Metodi hakee tietokannasta käyttäjien omatekemien tehtävien määrän
      * @return Palauttaa tehtävien määrän
      */
+    @Override
     public int getAmountOfCustomExercises() {
  
         try {
@@ -145,6 +152,7 @@ public class Database implements DataDao {
      * @param id Parametri on halutun kysymyksen numero
      * @return Palauttaa kysymyksen
      */
+    @Override
     public String getCustomExerciseQuestion(int id) {
         try {
             PreparedStatement ps = conn.prepareStatement("SELECT question FROM Custom WHERE id=?");
@@ -164,6 +172,7 @@ public class Database implements DataDao {
      * @param id Parametri on halutun vastauksen numero
      * @return Palauttaa vastauksen
      */
+    @Override
     public String getCustomExerciseAnswer(int id) {
         try {
             PreparedStatement ps = conn.prepareStatement("SELECT answer FROM Custom WHERE id=?");
@@ -184,6 +193,7 @@ public class Database implements DataDao {
      * @param answer Käyttäjän antama vastaus
      */
     
+    @Override
     public void createNewCustomExercise(String question, String answer) {
         int nro = this.getAmountOfCustomExercises();
         try {
@@ -202,6 +212,7 @@ public class Database implements DataDao {
      * Metodi lasekee mikä on käyttäjän tehtävien onnistumisprosentti.
      * @return Palauttaa lasektun  prosentin.-
      */
+    @Override
     public double getPercent() {
         return this.getPoints() * 100 / this.getExerciseCount();
     }
@@ -209,6 +220,7 @@ public class Database implements DataDao {
     /**
      * Metodilla voidaan poistaa taulut tietokannassa (vain testejä varten)
      */
+    @Override
     public void dropTables() {
         try {
             Statement s1 = conn.createStatement();
